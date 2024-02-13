65ef990805b545a6a2a669cf6ec2c54b2d8f71a4
Utsav Sharma <utsav19913@gmail.com>
2024-02-13 17:12:25 +0530

    added database config and initialzation

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+const mongodb = require("./config/mongodb");
 
 app.get("/", (req, res) => res.send("Hello world"));
+
+app.get("/health", (req, res) =>
+  res.json({
+    service: "Job listing server",
+    status: "Active",
+    time: new Date(),
+  })
+);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
